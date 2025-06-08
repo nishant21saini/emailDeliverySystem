2bc25ad6d0e8112fdbb3e76e874d521c492c98ca
nishant2102 <nishantsaini7811@gmail.com>
2025-06-08 22:35:01 +0530

    make changes

--- index.js
+++ index.js
@@ -1,47 +1,73 @@
 import express from 'express';
 import { EmailService } from './EmailService/emailservice.js';
+import { rateLimit } from 'express-rate-limit';
 
 const app = express();
 app.use(express.json());
 
 
+//hit api 3 times in span of 1 sec
+const sendEmailLimiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	limit: 5,
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+});
+
+// For /results route
+const resultsLimiter = rateLimit({
+	windowMs: 60 * 1000, // 1 minute
+	limit: 5,
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+});
+
+// Apply the rate limiting middleware to all requests.
 const emailService = new EmailService({});
 
 
 const queue = [];
 const results = [];
 
-
-app.post('/send-email', (req, res) => {
+app.post('/send-email', sendEmailLimiter, (req, res) => {
   const { to, subject, body } = req.body;
 
   if (!to || !subject || !body) {
     return res.status(400).json({ error: 'Missing required fields: to, subject, body' });
   }
 
-  const email = { to, subject, body, timestamp: Date.now() };
-
   const emailId = `${to}-${subject}-${body}`;
+  const email = { id: emailId, to, subject, body, timestamp: Date.now() };
 
+  const isDuplicateInQueue = queue.some(queuedEmail => queuedEmail.id === emailId);
+  if (isDuplicateInQueue) {
+    return res.json({ 
+      message: 'Email already queued', 
+      email
+    });
+  }
 
   if (emailService.sentEmails.has(emailId)) {
     return res.json({ message: 'Email already sent', email });
   }
 
   queue.push(email);
-  console.log('📩 Email queued:', to);
+  console.log('Email queued:', to);
   res.json({ message: 'Email queued successfully', email });
 });
 
 
-app.get('/results', (req, res) => {
+app.get('/results',resultsLimiter, (req, res) => {
+    if(results.length === 0){
+      res.json("No Email are Sent");
+    }
   res.json(results);
 });
 
 setInterval(async () => {
   if (queue.length === 0) return;
 
-  console.log('🚀 Processing queued emails...');
+  console.log('Processing queued emails');
 
   while (queue.length > 0) {
     const email = queue.shift();
